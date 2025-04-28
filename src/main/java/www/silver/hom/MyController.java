7039b4be3ab30590a0972ec877e71bee6acb4eda
EY <key2938@gamil.com>
2025-04-28 13:47:56 +0900

    1212

---
+++ src/main/java/www/silver/hom/MyController.java
@@ -0,0 +1,28 @@
+package www.silver.hom;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MyController {
+
+	@Resource(name="cname")
+	String name;
+
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String home(Model model) {
+		System.out.println("Name: " + name);
+		return "index";
+	}
+
+
+
+	
+	
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kkk
+  Date: 25. 4. 28.
+  Time: 오후 1:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java"
+         pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
